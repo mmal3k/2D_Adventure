29a821155d59aa3ee4e72003f27e96894d93a972
mmal3k <mmalekrais@gmail.com>
2025-07-11 23:58:09 +0200

    add game panel

---
+++ src/main/GamePanel.java
@@ -0,0 +1,27 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel{
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16 * 16 tile
+    final int scale = 3 ;
+
+    final int tileSize = originalTileSize * scale ; // 48 * 48 tile
+
+    final int maxScreenCol = 16 ;
+    final int maxScreenRow = 12 ;
+
+    final int screenWidth = tileSize * maxScreenCol ; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow ; // 576 pixels
+
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth , screenHeight));
+        this.setBackground(Color.BLACK);
+        // make this true for better rendering performance
+        this.setDoubleBuffered(true);
+    }
+
+}
