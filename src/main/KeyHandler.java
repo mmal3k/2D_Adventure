ef0a2887cb01fa66d64a6d73012d5e1cf0fbb91c
mmal3k <mmalekrais@gmail.com>
2025-09-03 17:43:26 +0200

    display character status

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -20,103 +20,116 @@ public class KeyHandler implements KeyListener {
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
 
-        if (gp.gameState == gp.titleState) {
-            if (gp.ui.titleScreenState == 0) {
-                if(code == KeyEvent.VK_W ) {
-                    if (gp.ui.commandNum == 2) {
-                        gp.ui.commandNum = 0;
-                    }else {
-                        gp.ui.commandNum ++;
-                    }
-                }
-                if(code ==  KeyEvent.VK_S) {
-                    if (gp.ui.commandNum == 0) {
-                        gp.ui.commandNum = 2;
-                    }else {
-                        gp.ui.commandNum --;
-                    }
-                }
-                if(code ==  KeyEvent.VK_ENTER) {
-                    switch (gp.ui.commandNum){
-                        case 0 :
-                            gp.ui.titleScreenState = 1;
-//                            gp.gameState = gp.playState;
-//                            gp.playMusic(0);
-                            break;
-                        case 1 :
-//                    add later
-                            break;
-                        case 2 :
-
-                            System.exit(0);
-                            break;
-                    }
-                }
-            }
-
-            else if (gp.ui.titleScreenState == 1) {
-                if(code == KeyEvent.VK_W ) {
-                    if (gp.ui.commandNum == 3) {
-                        gp.ui.commandNum = 0;
-                    }else {
-                        gp.ui.commandNum ++;
-                    }
-                }
-                if(code ==  KeyEvent.VK_S) {
-                    if (gp.ui.commandNum == 0) {
-                        gp.ui.commandNum = 3;
-                    }else {
-                        gp.ui.commandNum --;
-                    }
-                }
-                if(code ==  KeyEvent.VK_ENTER) {
-                    switch (gp.ui.commandNum){
-                        case 0 , 1, 2 :
-                            gp.gameState = gp.playState;
-                            gp.playMusic(0);
-                            break;
-                        case 3 :
-                            gp.ui.commandNum = 0;
-                            gp.ui.titleScreenState = 0;
-                            break;
-                    }
-                }
-            }
+        if (gp.gameState == gp.titleState) {titleState(code);}
+        else if (gp.gameState == gp.playState) {playState(code);}
+        else if (gp.gameState == gp.pauseState) {pauseState(code);}
+        else if (gp.gameState == gp.dialogueState) {dialogueState(code);}
+        else if (gp.gameState == gp.characterState) {characterState(code);}
+    }
 
 
-        }
-
-        else if (gp.gameState == gp.playState) {
-            if(code == KeyEvent.VK_W) {
-                upPressed  = true ;
-            }
-            if(code ==  KeyEvent.VK_A) {
-                leftPressed = true ;
+    public void titleState(int code) {
+        if (gp.ui.titleScreenState == 0) {
+            if(code == KeyEvent.VK_W ) {
+                if (gp.ui.commandNum == 2) {
+                    gp.ui.commandNum = 0;
+                }else {
+                    gp.ui.commandNum ++;
+                }
             }
             if(code ==  KeyEvent.VK_S) {
-                downPressed = true ;
+                if (gp.ui.commandNum == 0) {
+                    gp.ui.commandNum = 2;
+                }else {
+                    gp.ui.commandNum --;
+                }
             }
-            if(code ==  KeyEvent.VK_D) {
-                rightPressed = true ;
-            }
-            if(code ==  KeyEvent.VK_P) {
-                gp.gameState = gp.pauseState;
-            }
-            if(code == KeyEvent.VK_ENTER) {
-                enterPressed = true;
-            }
-            if(code == KeyEvent.VK_T) {
-                checkDrawTime = !checkDrawTime ;
-            }
-
-        } else if (gp.gameState == gp.pauseState) {
-            if(code ==  KeyEvent.VK_P) {
-                gp.gameState = gp.playState;
-            }
-        } else if (gp.gameState == gp.dialogueState) {
             if(code ==  KeyEvent.VK_ENTER) {
-                gp.gameState = gp.playState;
+                switch (gp.ui.commandNum){
+                    case 0 :
+                        gp.ui.titleScreenState = 1;
+//                            gp.gameState = gp.playState;
+//                            gp.playMusic(0);
+                        break;
+                    case 1 :
+//                    add later
+                        break;
+                    case 2 :
+
+                        System.exit(0);
+                        break;
+                }
             }
+        }
+
+        else if (gp.ui.titleScreenState == 1) {
+            if(code == KeyEvent.VK_W ) {
+                if (gp.ui.commandNum == 3) {
+                    gp.ui.commandNum = 0;
+                }else {
+                    gp.ui.commandNum ++;
+                }
+            }
+            if(code ==  KeyEvent.VK_S) {
+                if (gp.ui.commandNum == 0) {
+                    gp.ui.commandNum = 3;
+                }else {
+                    gp.ui.commandNum --;
+                }
+            }
+            if(code ==  KeyEvent.VK_ENTER) {
+                switch (gp.ui.commandNum){
+                    case 0 , 1, 2 :
+                        gp.gameState = gp.playState;
+                        gp.playMusic(0);
+                        break;
+                    case 3 :
+                        gp.ui.commandNum = 0;
+                        gp.ui.titleScreenState = 0;
+                        break;
+                }
+            }
+        }
+    }
+    public void playState(int code) {
+        if(code == KeyEvent.VK_W) {
+            upPressed  = true ;
+        }
+        if(code ==  KeyEvent.VK_A) {
+            leftPressed = true ;
+        }
+        if(code ==  KeyEvent.VK_S) {
+            downPressed = true ;
+        }
+        if(code ==  KeyEvent.VK_D) {
+            rightPressed = true ;
+        }
+        if(code ==  KeyEvent.VK_P) {
+            gp.gameState = gp.pauseState;
+        }
+        if(code == KeyEvent.VK_ENTER) {
+            enterPressed = true;
+        }
+        if(code == KeyEvent.VK_T) {
+            checkDrawTime = !checkDrawTime ;
+        }
+        if(code == KeyEvent.VK_C) {
+            gp.gameState = gp.characterState;
+        }
+    }
+    public void pauseState(int code) {
+        if(code ==  KeyEvent.VK_P) {
+            gp.gameState = gp.playState;
+        }
+    }
+    public void dialogueState(int code) {
+        if(code ==  KeyEvent.VK_ENTER) {
+            gp.gameState = gp.playState;
+        }
+    }
+    public void characterState(int code) {
+        if (code == KeyEvent.VK_C) {
+            gp.gameState = gp.playState ;
         }
     }
 
--- src/main/UI.java
+++ src/main/UI.java
@@ -53,18 +53,17 @@ public class UI {
         if (gp.gameState == gp.playState) {
             drawPlayerLife();
         }
-
         if (gp.gameState == gp.pauseState) {
             drawPlayerLife();
             drawPauseScreen ();
         }
-
         if (gp.gameState == gp.dialogueState) {
             drawPlayerLife();
             drawDialogScreen();
         }
-
-
+        if(gp.gameState == gp.characterState) {
+            drawCharaterScreen();
+        }
     }
 
     public void drawPlayerLife() {
@@ -120,20 +119,113 @@ public class UI {
         width = gp.screenWidth  - (gp.tileSize*4) ;
         height = gp.tileSize * 4;
         drawSubWindow(x ,y ,width , height);
-
-
-
-
         x += gp.tileSize;
         y += gp.tileSize;
-
-
         for (String line : currentDialog.split("\n")) {
             g2.drawString(line , x ,y);
             y += 40 ;
         }
+    }
+
+    public void drawCharaterScreen() {
+        // Create a frame
+        final  int frameX = gp.tileSize;
+        final  int frameY = gp.tileSize;
+        final  int frameWidth = gp.tileSize *5  ;
+        final  int frameHeight = gp.tileSize * 10 ;
+
+        drawSubWindow(frameX , frameY , frameWidth , frameHeight);
+
+
+//        Text
+        g2.setColor(Color.white);
+        g2.setFont(g2.getFont().deriveFont(32F));
+
+        int textX  = frameX + 20 ;
+        int textY  = frameY + gp.tileSize;
+        final int lineHeight = 36 ;
+
+//        Names
+        g2.drawString("Level" , textX , textY );
+        textY += lineHeight;
+        g2.drawString("Life", textX , textY);
+        textY += lineHeight;
+        g2.drawString("Strength", textX , textY);
+        textY += lineHeight;
+        g2.drawString("Dexterity", textX , textY);
+        textY += lineHeight;
+        g2.drawString("Attack", textX , textY);
+        textY += lineHeight;
+        g2.drawString("Defense", textX , textY);
+        textY += lineHeight;
+        g2.drawString("Exp", textX , textY);
+        textY += lineHeight;
+        g2.drawString("Next Level", textX , textY);
+        textY += lineHeight;
+        g2.drawString("Coin", textX , textY);
+        textY += lineHeight+20;
+        g2.drawString("Weapon", textX , textY);
+        textY += lineHeight+15;
+        g2.drawString("Shield", textX , textY);
+        textY += lineHeight;
+
+//        Values
+        int tailX = frameX + frameWidth - 30;
+//        Reset text Y
+        textY = frameY  + gp.tileSize ;
+        String value = "";
+
+        value = String.valueOf(gp.player.level);
+        textX = getXForAlignToRightText(value , tailX);
+        g2.drawString(value , textX , textY);
+        textY += lineHeight;
+
+        value = String.valueOf(gp.player.life + "/" + gp.player.maxLife);
+        textX = getXForAlignToRightText(value , tailX);
+        g2.drawString(value , textX , textY);
+        textY += lineHeight;
+
+        value = String.valueOf(gp.player.strength);
+        textX = getXForAlignToRightText(value , tailX);
+        g2.drawString(value , textX , textY);
+        textY += lineHeight;
+
+        value = String.valueOf(gp.player.dexterity);
+        textX = getXForAlignToRightText(value , tailX);
+        g2.drawString(value , textX , textY);
+        textY += lineHeight;
+
+        value = String.valueOf(gp.player.attack);
+        textX = getXForAlignToRightText(value , tailX);
+        g2.drawString(value , textX , textY);
+        textY += lineHeight;
+
+        value = String.valueOf(gp.player.defense);
+        textX = getXForAlignToRightText(value , tailX);
+        g2.drawString(value , textX , textY);
+        textY += lineHeight;
+
+        value = String.valueOf(gp.player.exp);
+        textX = getXForAlignToRightText(value , tailX);
+        g2.drawString(value , textX , textY);
+        textY += lineHeight;
+
+        value = String.valueOf(gp.player.nextLevelExp);
+        textX = getXForAlignToRightText(value , tailX);
+        g2.drawString(value , textX , textY);
+        textY += lineHeight;
+
+        value = String.valueOf(gp.player.coin);
+        textX = getXForAlignToRightText(value , tailX);
+        g2.drawString(value , textX , textY);
+        textY += lineHeight;
+
+        g2.drawImage(gp.player.currentWeapon.down1, tailX - gp.tileSize , textY - 14 , null);
+        textY += gp.tileSize;
+        g2.drawImage(gp.player.currentShield.down1, tailX - gp.tileSize , textY - 14 , null);
 
     }
+
 
 
     public int getXForCenteredText (String text) {
@@ -250,6 +342,11 @@ public class UI {
 
     }
 
+    public int getXForAlignToRightText(String text , int tailX) {
+        int length = (int)g2.getFontMetrics().getStringBounds(text , g2).getWidth();
+        int x = (tailX - length) ;
+        return  x ;
+    }
 
 }
 
